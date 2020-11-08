2d5da5cf51850223587fcc166aa06edd4ac789e1
Joshua Rogan <joshua.rogan@mydit.ie>
2020-11-08 21:54:59 +0000

    home, signup, index. base etc done

--- world/views.py
+++ world/views.py
@@ -1,5 +1,45 @@
-from django.shortcuts import render
+from geodjango.models import Profile
+from django.http import JsonResponse
+from django.contrib.auth.decorators import login_required
+from django.contrib.gis.geos import Point
+from django.contrib.auth import login, authenticate
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+
+def home(request):
+    return render(request, 'home.html')
+
+def signup(request):
+    form = UserCreationForm(request.POST)
+    if form.is_valid():
+        form.save()
+        username = form.cleaned_data.get('username')
+        password = form.cleaned_data.get('password1')
+        user = authenticate(username=username, password=password)
+        login(request, user)
+        return redirect('home')
+    return render(request, 'signup.html', {'form': form})
+
+@login_required
+def update_location(request):
+    try:
+        user_profile = Profile.objects.get(user=request.user)
+        if not user_profile:
+            raise ValueError("Can't get User details")
+
+        logger.error(user_profile)
+
+        point = request.POST["point"].split(",")
+        point = [float(part) for part in point]
+        point = Point(point, srid=4326)
+
+        user_profile.last_location = point
+        user_profile.save()
+
+        return JsonResponse({"message": f"Set location to {point.wkt}."}, status=200)
+    except Exception as e:
+        return JsonResponse({"message": str(e)}, status=400)
