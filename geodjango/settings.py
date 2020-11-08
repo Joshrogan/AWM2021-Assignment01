b8bb98ced3e5895a862478f51ef53fd53b317605
Joshua Rogan <joshua.rogan@mydit.ie>
2020-11-08 21:08:09 +0000

    settings updated

--- geodjango/settings.py
+++ geodjango/settings.py
@@ -9,9 +9,9 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
+import sys, os
 from pathlib import Path
-from python_config import KEY
+from .python_config import *
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
