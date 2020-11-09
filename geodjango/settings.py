d6eb6efa4ae10fade845a574c16deec884449443
Joshua Rogan <joshua.rogan@mydit.ie>
2020-11-09 00:40:08 +0000

    site working completely

--- geodjango/settings.py
+++ geodjango/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = DEBUGSTATUS
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '[::1]']
+ALLOWED_HOSTS = HOSTS
 
 
 # Application definition
